2dd08f98fc2b5042d7779e64074f5891c71a5b3b
JulioPrado <18661265+JulioPrado@users.noreply.github.com>
2020-08-03 21:17:41 -0300

    atualiza layout, herois, etc

--- js/script.js
+++ js/script.js
@@ -1,48 +1,159 @@
 
-var Hero = function(){
-	this.compravel=
+  
+// CLASSES  =====================================================================================
+
+function hero(nome,lvl,maxhp,maxmana,atqDano,esquiva,forcaEspecial,especial){
+  this.posicao=null;
+  this.nome=nome;
+  //this.natureza=natureza;
+  this.natureza=null;
+  //this.raca=raca;
+  this.raca=null;
+  //this.classe=classe;
+  this.classe=null;
+  //this.origem=origem;
+  this.origem=null;
+  //this.planeta=planeta;
+  this.planeta=null;
+  this.lvl=lvl;
+  this.maxhp=maxhp;
+  this.maxmana=maxmana;
+  this.atqDano=atqDano;
+  this.esquiva=esquiva;
+  this.forcaEspecial=forcaEspecial;
+  this.especial=especial;
+  this.chanceCritico=10;
+
+  function atacar(alvo){
+
+  }
+
+
 }
-
-
-
-
-
-var herois = []
 /*
-0-Compravel,1-cod,2-nome,3-classe,4-vidaMax,5-manaMax,6-Alcance(0,1),7-velAtk(Lento,Normal,Rapido),8-danoATK(n),9-tipoAtk(Fisico,Magico,Real),10-armadura,11-resistMagica,12-Habi(str),13-imagem,14-imagemMorto,15-posI,16-posJ
+0 posicao
+1 nome
+2 natureza-
+3 raça-
+4 classe-
+5 origem-
+6 planeta-
+
+7 level
+8 maxhp
+9 maxmana
+10 atqDano
+11 esquiva
+12 forcaEspecial
+13 especial (func)
+14 chanceCritico (padrão 10)
 */
-var sacopancada = [0,0,'Saco de pancada','Treino',999,0,0,'Lento',0,'Fisico',1,1,"Não tem habilidade.",'imagens/proto.png','imagens/protom.png',0,0]
-var proto = [1,1,'Proto','Prototipo',10,10,0,'Normal',1,'Fisico',1,1,"O próximo ataque de Proto causa o triplo do dano.",'imagens/proto.png','imagens/protom.png',0,0]
-var cavaleiroDeLica = [1,2,'Cavaleiro de Liça','Cavaleiro',10,10,0,'Normal',1,'Fisico',1,1,"O Cavaleiro de Liça avança, atacando e reduzindo a armadura do alvo em 5.",'imagens/CavaleiroDeLiça.png','imagens/protom.png',0,0]
-var steroid = [1,3,'Steroid','Lutador',10,10,0,'Normal',1,'Fisico',1,1,"Steroid se cura em 50 e recebe 2 de armadura e resistencia magica.",'imagens/Steroid.png','imagens/protom.png',0,0]
+function especial(especialnome,func){
+  this.especialnome=especialnome;
+  this.func=func;
+}
 
+// ESPECIAIS DOS HERÓIS  ========================================================================
 
-var arena = [];
+var purpleEspecial = new especial("Lança do trovão",
+  function purpleEspecial(inimigoPos,forca){
+  
+})
 
-var jogador = []
-jogador.push(["Nome",1,500]);
+var protoEspecial = new especial("Machado voraz",
+  function protoEspecial(forca){
+  console.log("Especial do Proto ativado com força: "+forca);
+})
 
-var reservas = []
-reservas.push(proto)
-reservas.push(cavaleiroDeLica)
-reservas.push(steroid)
+var redEspecial = new especial("Flecha negra",
+  function protoEspecial(forca){
+  console.log("Especial do Red ativado com força: "+forca);
+})
 
-var time = []
+var steroidEspecial = new especial("Cometa de pégasuuuu",
+  function steroidEspecial(forca){
+  console.log("Especial do Steroid ativado com força: "+forca);
+})
 
+// HERÓIS  ======================================================================================
+/*
+0 posicao
+1 nome
 
-var timeInimigo= []
+2 natureza-
+3 raça-
+4 classe-
+5 origem-
+6 planeta-
 
-console.table(reservas);
+7 level
+8 maxhp
+9 maxmana
+10 atqDano
+11 esquiva
+12 forcaEspecial
+13 especial (func)
+14 chanceCritico (padrão 10)
+*/
+var purple = new hero("Purple",1,100,10,5,1,10,purpleEspecial);
+var proto = new hero("Proto",1,100,10,5,1,10,protoEspecial);
+var red = new hero("Red",1,90,15,8,1,12,redEspecial);
+var steroid = new hero("Steroid",1,120,10,5,1,10,steroidEspecial);
 
+// LOJA ====================================================================================
+var loja = [];
 
-function mudarBarra(id,mudanca) {
-	d= document.getElementById(id);
-	console.log(parseInt(d.width));
-	d.width=parseInt(d.width)+mudanca;
+loja.push(Object.assign({}, purple));
+loja.push(Object.assign({}, proto));
+loja.push(Object.assign({}, red));
+loja.push(Object.assign({}, steroid));
+
+function montarLoja(){
+	
+}
+
+// TIMES  ======================================================================================
+
+var meuTime = [];
+var banco =[];
+var inimigoTime =[];
+
+// TESTES  ======================================================================================
+
+function colocarBanco(indiceTime){
+  banco.push(Object.assign({}, meuTime[indiceTime] ));
+
+  meuTime[indiceTime]=null
+
+  for (let i = indiceTime; i<meuTime.length-1 ;i++) {
+    if (meutime[i]==null) {
+      meutime[i]=Object.assign({}, meuTime[i+1] );
+      meutime[i+1]=null;
+    }
+  }
+}
+
+function colocarTime(indiceBanco){
 
 }
 
-function definirAlvo(timeAlvo){
+// teste de chamada de especial passando a força como parâmetro meuTime[2].especial.func(meuTime[2].forcaEspecial)
+console.table(loja)
+console.table(meuTime)
+console.table(banco)
+
+
+// COMBATE ====================================================================================
+
+ function selecionaTimeInimigo(nivelArena){
+  if (nivelArena<100) {
+
+  }else{
+
+  }
+ }
+
+  function definirAlvo(timeAlvo){
 	if (timeAlvo==1) {
 		for (var atkj = 1; atkj>=0; atkj--){
 			for ( atki=0;atki<4 ;atki++){
@@ -70,13 +181,4 @@ function definirAlvo(timeAlvo){
 	}else{
 		console.log("Time alvo inválido.")
 	}
-}
-
-function atacar(origemAtk,tipoAtk,danoAtk,alvoAtk){
-
-		
-}
-
-function atribuirPos(time){
-
 }
